c68b27c94f3d21a36e7420ba6c8ff255a6089f9e
Matthew Healy <mhealy@embyrtech.io>
2017-11-29 08:25:16 -0600

    add descriptive comments

--- fast_addr.asm
+++ fast_addr.asm
@@ -8,26 +8,28 @@
   LOAD: MOV R4, @R6    ; temp hold for byte of R6 data
         XRL @R6, @R7   ; propagate
         ANL @R7, R4    ; generate
-        INC R6
-        INC R7
+        INC R6         ; move to next bit of P
+        INC R7         ; move to next bit of G
         DNJZ R5, LOAD
 
         MOV R5, R2     ; reset R5
         MUL R5, #8H    ; switch to bit counter
-        MOV R5, A
-        CLR C
+        MOV R5, A      ; load counter with length*8
+        CLR C          ; C will be used as Ci in boolean equation
         MOV R6, #040H  ; bit addr
         MOV R7, #070H  ; bit addr
- CARRY: ANL C, @R6
-        ORL C, @R7
-        MOV @R3, C
-        INC R3
-        INC R6
-        INC R7
+
+ CARRY: ANL C, @R6     ; intermediate = Ci AND P(i+1)
+        ORL C, @R7     ; C(i+1) = intermediate OR G(i+1)
+        MOV @R3, C     ; store C(i+1) in carry string
+        INC R3         ; move to next bit of carry string
+        INC R6         ; move to next bit of P
+        INC R7         ; move to next bit of G
         DJNZ R5, CARRY
 
-        MOV R5, #8H
-   SUM: XRL @R6 @R3
-        INC R6
-        INC R3
-        DJNZ R5, SUM   ; result is in @R6
+        MOV R5, R2     ; reload counter with length
+   SUM: XRL @R6 @R3    ; compute final sum
+        INC R6         ; move to next bit of P
+        INC R3         ; move to next bit of Carry string
+        DJNZ R5, SUM
+        RET            ; result is in what was P string
