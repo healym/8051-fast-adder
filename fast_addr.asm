c735717919e0557d919c304a481871f7fbe08eef
Matthew Healy <mhealy@embyrtech.io>
2017-11-28 18:17:10 -0600

    add basic architecture

---
+++ fast_addr.asm
@@ -0,0 +1,32 @@
+; kiel compiler
+
+ORG YYYY
+        MOV R3, #40H ; #10H bit
+        MOV R6, #48H ; #40H bit
+        MOV R7, #50H ; #70H bit
+
+  LOAD: MOV R4, @R6    ; temp hold for byte of R6 data
+        XRL @R6, @R7   ; propagate
+        ANL @R7, R4    ; generate
+        INC R6
+        INC R7
+        DNJZ R5, LOAD
+
+        MUL R5, #8H    ; need to set R5***
+        MOV R5, A
+        CLR C
+        MOV R6, #040H  ; bit addr
+        MOV R7, #070H  ; bit addr
+ CARRY: ANL C, @R6
+        ORL C, @R7
+        MOV @R3, C
+        INC R3
+        INC R6
+        INC R7
+        DJNZ R5, CARRY
+
+        MOV R5, #8H
+   SUM: XRL @R6 @R3
+        INC R6
+        INC R3
+        DJNZ R5, SUM   ; result is in @R6
